2b150335fcc5d16379593a04f651a97ea107bc32
Alec Gorge <alecgorge@gmail.com>
2023-09-21 10:59:07 -0700

    make the git processing much faster

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,14 +1,6 @@
 
-function flash_git_is_stashed
-  command git rev-parse --verify --quiet refs/stash >/dev/null
-end
-
 function flash_git_branch_name
-  command git symbolic-ref --short HEAD
-end
-
-function flash_git_is_touched
-  test -n (echo (command git status --porcelain))
+  command git for-each-ref --format='%(objectname) %(refname:short)' refs/heads | awk "/^$(git rev-parse HEAD)/ {print \$2}"
 end
 
 function fish_right_prompt
@@ -18,19 +10,12 @@ function fish_right_prompt
     test $code -ne 0; and echo (flash_snd); or echo (flash_fst)
   end
 
-  if test $CMD_DURATION -gt 1000
-    printf (flash_dim)" ~"(printf "%.1fs " (math "$CMD_DURATION / 1000"))(flash_off)
+  if test $CMD_DURATION -gt 0
+    printf (flash_dim)" "(printf "%.3fs " (math "$CMD_DURATION / 1000"))(flash_off)
   end
 
   if test -d .git
-    if flash_git_is_stashed
-      echo (flash_dim)"<"(flash_off)
-    end
-    printf (begin
-      flash_git_is_touched
-        and echo (flash_fst)"(*"(flash_snd)(flash_git_branch_name)(flash_fst)")"(flash_off)
-        or echo (flash_snd)"("(flash_fst)(flash_git_branch_name)(flash_snd)")"(flash_off)
-    end)(flash_off)
+    echo (flash_snd)"("(flash_fst)(flash_git_branch_name)(flash_snd)")"(flash_off)
   end
 
   printf " "(flash_trd)(date +%H(status::color):(flash_dim)%M(status::color):(flash_trd)%S)(flash_snd)" "(flash_off)
